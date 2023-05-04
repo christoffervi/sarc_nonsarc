6ad1e7761bd2eba9afbe70f6a7161048e8c142a9
Christoffer Vissing <christoffervi@gmail.com>
2023-05-03 22:25:58 -0400

    manuscript

--- Figure 5- triplets.R
+++ Figure 5- triplets.R
@@ -1,4 +1,3 @@
-setwd("~/Dropbox/10 R/non-genetic hcm/sarc_nonsarc/")
 df_long <- df %>%
   mutate(age_hcm = primary_diagnosis_age, # renaming variable
          age_htxvad = pmin(age_vad, age_transplant, na.rm = T), #merging vad and HTX
@@ -45,6 +44,7 @@ df_long <- df_long %>%
 #Diagnostic triplets
 triplets <-
   df_long %>% 
+  filter(sarc_status %in% c("SARC(+)", "SARC(-)")) %>%
   select(!previous_event) %>% 
   pivot_wider(names_from = rank, values_from = c(outcome, age)) %>%
   mutate(vuf1 = paste(outcome_1,outcome_2,outcome_3,age_1,age_2,age_3, sep = ";"),
@@ -87,15 +87,16 @@ triplets <-
                           T~destination)) 
 
 
-
+triplets %>% 
+  filter(sarc_status %in% c("SARC(+)", "SARC(-)")) %>%
+  group_by(triplet) %>% filter(row_number()==1) %>% arrange(desc(triplet_frequency)) %>% 
+  ggplot(aes(x= triplet_frequency))+
+  geom_histogram(binwidth = 1)
 
 triplets %>% 
   filter(sarc_status %in% c("SARC(+)", "SARC(-)")) %>%
   filter(rank<11) %>%  
   group_by(triplet) %>% mutate(min_start = min(value)) %>% ungroup() %>% 
-  #  mutate(rank = dense_rank(desc(triplet_frequency)),
-  #         age_rank = dense_rank(desc(min_start))
-  #  ) %>%
   mutate(age_rank = dense_rank(desc(min_start)),
          name = case_when(name=="arrhythmia_nsvt"~"NSVT",
                           name=="obstruction"~"LV obstruction",
@@ -146,3 +147,76 @@ triplets %>%
   coord_cartesian(xlim = c(36,66), ylim = c(0.4,16.6), expand = F)
 facet_wrap(~sarc_status, scales = "free_x")
 ggsave(filename = "Triplets.tiff", compression = "lzw", height = 16, width = 18, units = "cm", dpi =1200)
+
+
+###########
+# This section includes a relative risk of triplet-events
+#######
+triplets %>% ungroup() %>% 
+  # filter(rank<=10) %>% 
+  select(sarc_status,triplet, n) %>% 
+  mutate(sarc_status = if_else(sarc_status=="SARC(+)","sarc","non")) %>%
+  group_by(triplet, sarc_status) %>% filter(row_number()==1) %>% 
+  ungroup() %>% 
+  pivot_wider(names_from = sarc_status, values_from = n) %>% 
+  arrange(desc(sarc)) %>% #drop_na() %>% #filter(non>1 & sarc>1) %>% 
+  mutate(RR = (non/2739)/(sarc/2715)) %>% 
+  filter(sarc>=14 | non>=20) %>% 
+  select(triplet,RR) %>% left_join(triplets) %>% filter(rank<11) %>% 
+  group_by(triplet) %>% mutate(min_start = min(value)) %>% ungroup() %>% 
+  mutate(age_rank = dense_rank(desc(min_start)),
+         name = case_when(name=="arrhythmia_nsvt"~"NSVT",
+                          name=="obstruction"~"LV obstruction",
+                          name=="srt"~"SRT",
+                          name=="htn"~"Hypertension",
+                          name=="icd"~"ICD",
+                          name=="nyha_hf"~"NYHA III-IV",
+                          name=="af"~"Atrial Fibrillation",
+                          #name=="nyha_hf"~"HF symptoms",
+                          T~name),
+         name = fct_infreq(name),
+  ) %>% group_by(triplet) %>% mutate(rr= if_else(row_number()==1, round(1/RR,2), NA_real_)) %>% ungroup() %>% 
+  left_join(trip_rr_ci) %>% 
+  group_by(triplet) %>% mutate(across(c(.est,.lower,.upper), ~if_else(row_number()==1, round(1/.x,2), NA_real_)),
+                               rr_ci = paste(.est," (CI ", .upper, "-", .lower,")", sep = ""),
+                               rr_ci = if_else(str_detect(rr_ci, "NA"), NA_character_,rr_ci)) %>% ungroup() %>% 
+  
+  ggplot(aes(y=age_rank, x= value, color = name, yend =age_rank, xend =end))+
+  geom_text(aes(label = paste(n), x=end+.5), color = "black", size = 3, hjust = 0)+
+  geom_segment(color= "black")+
+  scale_y_discrete(breaks = seq(0,25,1))+
+  scale_x_continuous(breaks = seq(0,66,2))+
+  scale_color_manual(name=NULL ,values = c(ggsci::pal_jama()(7)))+
+  
+  labs(x= "Mean age at occurence",
+       y= "")+
+  scale_size(range = c(2,5), name = NULL, breaks = NULL)+
+  scale_shape_manual(values = c(16,18))+
+  #scale_color_scico_d(palette = "vanimo")+
+  #geom_text(aes(x=10, label = source))+
+  geom_point(aes(size = 1/rank, shape = sarc_status),  show.legend = T)+
+  theme(panel.background = element_rect(fill = "white", color = "black"),
+        axis.text.x = ggtext::element_markdown(family = "Roboto", color = "black", size = 10),
+        axis.text.y =element_blank(),# element_markdown(family = "Roboto", color = "black"),
+        axis.title = ggtext::element_markdown(family = "Roboto", color = "black"),
+        panel.grid.major = element_line(color = "gray89"),
+        axis.ticks.y = element_blank(),
+        strip.text = element_text(family = "Roboto", color = "white", face = "bold", size = 12),
+        strip.background = element_rect(fill = "#be1e32"),
+        legend.position = #c(.6,.2),
+          "bottom",
+        legend.direction = #"vertical",
+          "horizontal",
+        legend.key.size = unit(1, "lines"),
+        legend.key = element_blank(),
+        legend.title = element_blank(),
+        legend.background = element_rect(fill = "gray99", #color = "black", linetype = 1
+        ),
+        legend.text = element_text(family = "Roboto", color = "black", size = 10)
+  )+
+  guides(shape = guide_legend(override.aes = list(size = 4)),
+         color = guide_legend(override.aes = list(size = 4)))+
+  coord_cartesian(xlim = c(36,71), ylim = c(0.4,18.6), expand = F)+
+  geom_text(aes(x=68.5, label = rr), color = "black", family = "Helvetica")+
+  annotate("text", x= 68.5, y=17.4, label = "Relative risk\nin sarc",
+           family = "Helvetica", fontface = "bold", vjust = 0.5, size = 3.5)
